7ac2c3a74f8442eb164eb1c75190728e563766f5
Mohdlateef <lateefahbaba123@gmail.com>
2024-10-25 19:18:05 +0530

    second example

---
+++ higherorderfunction.js
@@ -0,0 +1,24 @@
+let arr=[1,2,3,4,5]
+
+arr.forEach((element,index,list)=>{
+    console.log(element,index,list);
+})
+let obj={a:10,
+    b:30,
+    c:40,
+}
+
+
+let str="alhmadulilah i am the one who is choosen to be a webdeveloper";
+console.log(str);
+for(let i=0;i<str.length;i++)
+{
+    console.log(str[i]);
+}
+let str2=str.split(' ');
+console.log(str2);
+let str3=str2.join("");
+console.log(str3);
+let firstname="lateef";
+let lastname="baba";
+console.log(firstname +" "+ lastname);
